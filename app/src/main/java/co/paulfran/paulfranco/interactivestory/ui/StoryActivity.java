d32a8d132d1f25a7a57e1bf3562d3d98a307801a
Paul Franco <paulfranco@me.com>
2018-03-22 04:49:07 -0700

    Started setting up the rest of the layouts

---
+++ app/src/main/java/co/paulfran/paulfranco/interactivestory/ui/StoryActivity.java
@@ -0,0 +1,57 @@
+package co.paulfran.paulfranco.interactivestory.ui;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import co.paulfran.paulfranco.interactivestory.R;
+import co.paulfran.paulfranco.interactivestory.model.Page;
+import co.paulfran.paulfranco.interactivestory.model.Story;
+
+public class StoryActivity extends AppCompatActivity {
+
+    public static final String TAG = StoryActivity.class.getSimpleName();
+
+    private Story story;
+    private ImageView storyImageView;
+    private TextView storyTextView;
+    private Button choice1Button;
+    private Button choice2Button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        storyImageView = (ImageView)findViewById(R.id.storyImageView);
+        storyTextView = (TextView)findViewById(R.id.storyTextView);
+        choice1Button = (Button)findViewById(R.id.choice1Button);
+        choice2Button = (Button)findViewById(R.id.choice2Button);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra(getString(R.string.key_name));
+        if (name == null || name.isEmpty()) {
+            name = "Friend";
+        }
+        Log.d(TAG, name);
+
+        story = new Story();
+        loadPage(0);
+
+
+    }
+
+    private void loadPage(int pageNumber) {
+        Page page = story.getPage(pageNumber);
+
+        Drawable image = ContextCompat.getDrawable(this, page.getImageId());
+        storyImageView.setImageDrawable(image);
+    }
+}
